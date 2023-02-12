618925de38f776c0de42b761fd62488be564e94f
junmi-miu <khanhttk3@fpt.edu.vn>
2023-02-12 17:03:34 +0700

    fasdvxS

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    /**
+     * @Route("Route", name="RouteName")
+     */
+    public function FunctionName(): Response
+    {
+        return $this->render('$0.html.twig', []);
+    }
+
+?>
